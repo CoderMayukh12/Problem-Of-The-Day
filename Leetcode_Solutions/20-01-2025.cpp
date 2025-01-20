e94239475e3f1aa6d7cc1d05c28a33452c07b9d9
CoderMayukh12 <mayukhbhowmik.cse2022@nsec.ac.in>
2025-01-20 20:10:29 +0530

    2661. First Completely Painted Row or Column

--- Leetcode_Solutions/20-01-2025.cpp
+++ Leetcode_Solutions/20-01-2025.cpp
@@ -1,39 +1,39 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// class Solution
-// {
-// public:
-//     int firstCompleteIndex(vector<int> &arr, vector<vector<int>> &mat)
-//     {
-//         unordered_map<int, pair<int, int>> mpp;
-//         for (int i = 0; i < mat.size(); i++)
-//         {
-//             for (int j = 0; j < mat[i].size(); j++)
-//             {
-//                 int x = mat[i][j];
-//                 mpp[x] = {i, j};
-//             }
-//         }
-//         // Mapped SuccessFully
-//         int n = mat.size();
-//         int m = mat[0].size();
-//         vector<int> r(n, m);
-//         vector<int> c(m, n);
-//         for (int i = 0; i < arr.size(); i++)
-//         {
-//             int ele = arr[i];
-//             auto it = mpp[ele];
-//             int x = it.first;
-//             int y = it.second;
-//             r[x]--;
-//             c[y]--;
-//             if (!r[x] || !c[y])
-//                 return i;
-//         }
-//         return 0;
-//     }
-// };
-// int main()
-// {
-//     return 0;
-// }
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int firstCompleteIndex(vector<int> &arr, vector<vector<int>> &mat)
+    {
+        unordered_map<int, pair<int, int>> mpp;
+        for (int i = 0; i < mat.size(); i++)
+        {
+            for (int j = 0; j < mat[i].size(); j++)
+            {
+                int x = mat[i][j];
+                mpp[x] = {i, j};
+            }
+        }
+        // Mapped SuccessFully
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<int> r(n, m);
+        vector<int> c(m, n);
+        for (int i = 0; i < arr.size(); i++)
+        {
+            int ele = arr[i];
+            auto it = mpp[ele];
+            int x = it.first;
+            int y = it.second;
+            r[x]--;
+            c[y]--;
+            if (!r[x] || !c[y])
+                return i;
+        }
+        return 0;
+    }
+};
+int main()
+{
+    return 0;
+}
