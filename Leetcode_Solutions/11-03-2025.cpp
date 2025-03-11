9c3e8b5a6a415361dcd63a5c000672b7d4b107fb
CoderMayukh12 <mayukhbhowmik.cse2022@nsec.ac.in>
2025-03-11 21:41:48 +0530

    1358. Number of Substrings Containing All Three Characters

---
+++ Leetcode_Solutions/11-03-2025.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int numberOfSubstrings(string s)
+    {
+        int n = s.size();
+
+        int i = 0, j = 0, count = 0;
+
+        unordered_map<char, int> mpp;
+
+        while (j < n)
+        {
+            mpp[s[j]]++;
+
+            while (mpp['a'] >= 1 && mpp['b'] >= 1 && mpp['c'] >= 1)
+            {
+                count += (n - j);
+
+                mpp[s[i]]--;
+                i++;
+            }
+            j++;
+        }
+
+        return count;
+    }
+};
